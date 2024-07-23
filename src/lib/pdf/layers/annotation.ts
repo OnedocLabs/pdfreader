c82ea1177dee1cc7982815a9d0ef15a14df2c4ba
Titouan Launay <titouan.launay@lleedpartners.com>
2024-07-23 16:12:13 +0200

    Improves performance of annotation layer when hidden

--- src/lib/pdf/layers/annotation.ts
+++ src/lib/pdf/layers/annotation.ts
@@ -1,6 +1,8 @@
 import { AnnotationLayer } from "pdfjs-dist";
 import { useEffect, useRef } from "react";
 
+import { useVisibility } from "@/lib/viewport";
+
 import { usePDFLinkService } from "../links";
 import { usePDFPage } from "../page";
 import { cancellable } from "../utils";
@@ -20,8 +22,23 @@ export const useAnnotationLayer = (params: AnnotationLayerParams) => {
   const annotationLayerRef = useRef<HTMLDivElement>(null);
   const annotationLayerObjectRef = useRef<AnnotationLayer | null>(null);
   const linkService = usePDFLinkService();
+  const { visible } = useVisibility({
+    elementRef: annotationLayerRef,
+  });
 
   const { pdfPageProxy } = usePDFPage();
+
+  useEffect(() => {
+    if (!annotationLayerRef.current) {
+      return;
+    }
+
+    if (visible) {
+      annotationLayerRef.current.style.contentVisibility = "visible";
+    } else {
+      annotationLayerRef.current.style.contentVisibility = "hidden";
+    }
+  }, [visible]);
 
   useEffect(() => {
     if (!annotationLayerRef.current) {
