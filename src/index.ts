a0fabab42f7a3236e76fee0ffd72009507f0f215
Otis Sutton <chopfitzroy@gmail.com>
2024-11-23 22:02:53 +1300

    Export additional API for public consumption

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+// @NOTE
+// - Required for custom `AnnotationLayer`
+export { usePDFPage } from "@/lib/pdf/page";
+export { useVisibility } from "@/lib/viewport";
+export { usePDFLinkService } from "@/lib/pdf/links";
+
+// @NOTE
+// - Required for `Viewer`
+export { usePDFDocument } from "@/lib/pdf/document";
+
+// @NOTE
+// - Defaults
 export * from "@/components";
